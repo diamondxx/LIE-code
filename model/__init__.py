b6b0a797eeafc0e136fa66dabe9e40128f18e864
wangyh <37398726+diamondxx@users.noreply.github.com>
2024-11-04 23:06:29 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,12 +1,12 @@
-from .Restormer import Restormer
-from .DMPHN import *
-from .Restormer_two_input import *
-from .Restormer_two_input_DCN import *
-from .Restormer_three_input_DCN import *
-from .Restormer_two_input_DCN_UnShareWeight import *
-from .Restormer_three_input_DCN_UnShareWeight import *
+# from .Restormer import Restormer
+# from .DMPHN import *
+# from .Restormer_two_input import *
+# from .Restormer_two_input_DCN import *
+# from .Restormer_three_input_DCN import *
+# from .Restormer_two_input_DCN_UnShareWeight import *
+# from .Restormer_three_input_DCN_UnShareWeight import *
 from .pyramidTransformer import *
-from .UNet import *
-from .FPTN_wo_feature_pyramid import *
-from .pyramidTransformer_event import *
-from .pyramidTransformer_rgb import *
+# from .UNet import *
+# from .FPTN_wo_feature_pyramid import *
+# from .pyramidTransformer_event import *
+# from .pyramidTransformer_rgb import *
